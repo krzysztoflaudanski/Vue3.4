ecd76a9907d7742f26a7a08b78b79f15b1f84ae4
Krzysztof <laudanski.krzysztof86@gmail.com>
2024-05-29 12:48:40 +0200

    add server footer header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
-<template><router-view /></template>
+<template>
+<div class="container p-p-4">
+
+  <app-header />
+
+  <main>
+    <router-view />
+  </main>
+
+  <app-footer />
+
+</div>
+</template>
 
 <script>
-import './styles/global.scss'
+import AppHeader from '@/components/layout/theHeader'
+import AppFooter from '@/components/layout/theFooter'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { AppHeader, AppFooter }
 }
 </script>
 
-<style lang="scss">
 
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/layout/theHeader.vue
@@ -0,0 +1,11 @@
+<template>
+<header class="header p-mt-2 p-mb-2">
+    Header
+</header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
