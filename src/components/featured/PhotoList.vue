8b7987a897c92e6d819304357a3f4b771e2b4519
Krzysztof <laudanski.krzysztof86@gmail.com>
2024-06-04 13:36:38 +0200

    change PhotoList to compositionAPI

--- src/components/featured/PhotoList.vue
+++ src/components/featured/PhotoList.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
 <div class="photo-list p-grid">
     <PhotoSummary class="sm:col-6 lg:col-2 md:col-3" v-for="photo in photos" :key="photo._id" :id="photo._id"
         :title="photo.title" :description="photo.description" :author="photo.author" :src="photo.src"
@@ -26,6 +26,56 @@ export default {
         }
     }
 };
+</script>
+
+<style scoped>
+.photo-list {
+    display: flex;
+    flex-wrap: wrap;
+}
+</style> -->
+
+
+Jasne, oto Twój komponent przerobiony z Options API na Composition API:
+
+vue
+Skopiuj kod
+<template>
+<div class="photo-list p-grid">
+    <PhotoSummary class="sm:col-6 lg:col-2 md:col-3" v-for="photo in photos" :key="photo._id" :id="photo._id"
+        :title="photo.title" :description="photo.description" :author="photo.author" :src="photo.src"
+        :votes="photo.votes" :category="photo.category" @vote="handleVote" />
+</div>
+</template>
+
+<script>
+import { defineComponent, toRefs } from 'vue'
+import PhotoSummary from './PhotoSummary.vue'
+
+export default defineComponent({
+    name: 'PhotoList',
+    components: {
+        PhotoSummary
+    },
+    props: {
+        photos: {
+            type: Array,
+            required: true
+        }
+    },
+    setup(props, { emit }) {
+        const { photos } = toRefs(props)
+
+        const handleVote = (photoId) => {
+            emit('vote', photoId)
+        }
+
+        return {
+            photos,
+            handleVote
+        }
+    }
+})
 </script>
 
 <style scoped>
